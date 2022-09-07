429dd98db4a27bafca75968304898dbc76681940
Peter McClelland <peterwmcclelland@gmail.com>
2022-09-06 20:23:46 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Nav from "./components/Nav";
+
+function App() {
+
+  return (
+    <div>
+      <Nav></Nav>
+      <main>
+      
+      </main>
+    </div>
+  );
+}
+
+export default App;
