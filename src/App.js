96a059c8ea2f1e8fdf9ef88c3b486eea8d7d3dc8
Peter McClelland <peterwmcclelland@gmail.com>
2022-09-06 21:59:13 -0500

    footer/about

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Nav from "./components/Nav";
+import About from "./components/About";
+import Contact from "./components/Contact"
 
 function App() {
 
   return (
     <div>
       <Nav></Nav>
-      <main>
       
+      <main>
+      <About></About>
       </main>
+      <footer>
+        <Contact></Contact>
+      </footer>
     </div>
   );
 }
---
+++ src/components/Contact/index.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
+
+function Contact() {
+    const [categories] = useState([
+        { name: 'GitHub', description: 'GitHub link' },
+        { name: '971-205-4928', description: 'Phone number'}
+      ]);
+      const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+    return (
+        <section className="my-5" >
+          <h1 id="Contact">Contact</h1>
+          <div className="my-2">
+            <footer>
+            <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a
+              href="https://github.com/PeterwMcClelland" target='blank'
+            >
+                <li>
+              </li>
+            </a>
+          </li>
+          <li>
+            <span></span>
+          </li>
+        
+          {categories.map((category) => (
+            <li className={`mx-1 ${
+                currentCategory.name === category.name && ''
+                }`} key={category.name}>
+              <span
+                onClick={() => {
+                  setCurrentCategory(category)
+                }}
+              >
+                  {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+          </footer>
+          </div>
+        </section>
+      )
+}
+
+export default Contact
+
